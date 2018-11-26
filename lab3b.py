656d4ab921e562c9d5091eb97072eb08e7a6d936
Matthew Paterno <mpaterno@live.com>
2018-11-25 21:11:59 -0800

    Fixed some formatting so it will build.

--- lab3b.py
+++ lab3b.py
@@ -1,13 +1,15 @@
 import csv
 import sys
 
-def readCSV( inputFile ):
+
+def readCSV(inputFile):
     print("BBB")
     csvFile = open(inputFile, "r")
 
     blockSize = 0
     inodeSize = 0
 
+    # Use first string to determine type.
     for line in csvFile:
         currentLine = line.split(',')
 
@@ -20,39 +22,44 @@ def readCSV( inputFile ):
             print("aAAA")
         elif currentLine[0] == "INODE":
             inodeNumber = currentLine[1]
-            fileType    = currentLine[2]
-             checkInode( inodeNumber, fileType )
+            fileType = currentLine[2]
+            checkInode(inodeNumber, fileType)
         elif currentLine[0] == "INDIRECT":
+            checkIndirect()
     return
+
 
 def checkGroup():
     return
 
+
 def checkBfree():
     return
+
 
 def addInode(inodeNumber, fileType):
     freeList = []
     if fileType == '0':
-        freeList[inodeNumber] = 
+        freeList[inodeNumber] = 999999  # Random Number, please change.
 
     return
+
 
 def checkIndirect():
     return
 
+
 def parseInode(fileType):
 
-    if fileType != "f" || fileType != "d":
+    if fileType != "f" | | fileType != "d":
+
 
 def main():
     if len(sys.argv) != 2:
         sys.stderr.write("Error: incorrect number of arguments \n")
-        exit(1);
+        exit(1)
 
     readCSV(sys.argv[1])
-
-
 
 
 if __name__ == '__main__':
